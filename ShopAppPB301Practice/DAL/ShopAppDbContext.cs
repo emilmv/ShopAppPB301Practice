65a3deff6155e47f5082580c5c5f9d9a729034ad
emilmv <emilnm@code.edu.az>
2024-08-06 12:45:54 +0400

    Add project files.

---
+++ ShopAppPB301Practice/DAL/ShopAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopAppPB301Practice.Entities;
+
+namespace ShopAppPB301Practice.DAL
+{
+    public class ShopAppDbContext : DbContext
+    {
+        public DbSet<Group>Groups { get; set; }
+        public DbSet<Student>Students { get; set; }
+        public ShopAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ ShopAppPB301Practice/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopAppPB301Practice.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Point { get; set; }
+
+    }
+}
